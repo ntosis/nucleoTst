08dc9498aad1e2bd868ba1fe8f448d5800dd959f
Tosis Nikolaos <tosis.nikolaos@gmail.com>
2017-03-15 20:55:27 +0100

    A small gui is working!!

--- freeRTOS_blink_test/include/GUIConf.h
+++ freeRTOS_blink_test/include/GUIConf.h
@@ -37,7 +37,7 @@ Purpose     : Configures emWins abilities, fonts etc.
   * @attention
   *@attention
   *
-  * <h2><center>&copy; Copyright © 2016 STMicroelectronics International N.V. 
+  * <h2><center>&copy; Copyright ï¿½ 2016 STMicroelectronics International N.V. 
   * All rights reserved.</center></h2>
   *
   * Redistribution and use in source and binary forms, with or without 
@@ -81,7 +81,7 @@ Purpose     : Configures emWins abilities, fonts etc.
 *
 *       Multi layer/display support
 */
-#define GUI_NUM_LAYERS            2    // Maximum number of available layers
+#define GUI_NUM_LAYERS            1    // Maximum number of available layers
 
 /*********************************************************************
 *
@@ -111,7 +111,7 @@ Purpose     : Configures emWins abilities, fonts etc.
 *
 *         Configuration of available packages
 */
-#define GUI_SUPPORT_MOUSE             (1)    /* Support a mouse */
+#define GUI_SUPPORT_MOUSE             (0)    /* Support a mouse */
 #define GUI_WINSUPPORT                (1)    /* Use window manager */
 #define GUI_SUPPORT_MEMDEV            (1)    /* Memory device package available */
 #define GUI_SUPPORT_DEVICES           (1)    /* Enable use of device pointers */
--- freeRTOS_blink_test/src/main.c
+++ freeRTOS_blink_test/src/main.c
@@ -133,10 +133,10 @@ int main(void)
    osThreadDef(task_300ms, Task_300ms, osPriorityNormal, 0, 128);
    TaskHandle_300ms = osThreadCreate(osThread(task_300ms), NULL);
 
-  osThreadDef(task_10ms, Task_10ms, osPriorityRealtime, 0, 128);
+  osThreadDef(task_10ms, Task_10ms, osPriorityNormal, 0, 128);
    TaskHandle_10ms = osThreadCreate(osThread(task_10ms), NULL);
 
-   osThreadDef(task_500ms, Task_500ms, osPriorityNormal, 0, 128);
+   osThreadDef(task_500ms, Task_500ms, osPriorityRealtime, 0, 1024);
    TaskHandle_500ms = osThreadCreate(osThread(task_500ms), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
@@ -285,6 +285,7 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler */ 
 }
 
+void GUI_X_ErrorOut(const char * s){}
 #ifdef USE_FULL_ASSERT
 
 /**
